42c48d4f585eabedc3192a0629cbacb1792709b2
verbenich <vivian@erbenich.eu>
2016-04-20 11:11:51 +0200

    build current time

--- stageDisplay/public/js/main.js
+++ stageDisplay/public/js/main.js
@@ -1,2 +1,19 @@
-var d = new Date();
-var n = d.getHours();
+window.onload = function() {
+  (function () {
+      function checkTime(i) {
+          return (i < 10) ? "0" + i : i;
+      }
+
+      function startTime() {
+          var today = new Date(),
+              h = checkTime(today.getHours()),
+              m = checkTime(today.getMinutes()),
+              s = checkTime(today.getSeconds());
+          document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+          t = setTimeout(function () {
+              startTime()
+          }, 500);
+      }
+      startTime();
+  })();
+}
